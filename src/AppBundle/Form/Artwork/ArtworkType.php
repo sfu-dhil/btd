63af6ab7bcb8acbb985eb9e3232e24309304c3ea
Michael Joyce <ubermichael@gmail.com>
2019-03-04 10:53:53 -0800

    composer update and a bit of tidying.

--- src/AppBundle/Form/Artwork/ArtworkType.php
+++ src/AppBundle/Form/Artwork/ArtworkType.php
@@ -17,6 +17,7 @@ class ArtworkType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder->add('title');
         $builder->add('artworkCategory');
+        $builder->add('excerpt', CKEditorType::class);
         $builder->add('content', CKEditorType::class);
         $builder->add('materials', CKEditorType::class);
         $builder->add('copyright', CKEditorType::class);
