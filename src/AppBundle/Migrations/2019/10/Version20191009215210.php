fbd6651854c56d1a5fdd384e26aa72f8ba3b205b
Michael Joyce <ubermichael@gmail.com>
2019-10-09 14:54:18 -0700

    switch to migrations.

---
+++ src/AppBundle/Migrations/2019/10/Version20191009215210.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191009215210 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->addSql('ALTER TABLE flag_entity DROP FOREIGN KEY FK_E37C49B9919FE4E5');
+        $this->addSql('DROP TABLE flag');
+        $this->addSql('DROP TABLE flag_entity');
+    }
+
+    public function down(Schema $schema) : void
+    {
+    }
+}
