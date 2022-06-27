8860993ad4d95dedc087ac29c7a4fc9f40105757
Michael Joyce <mjoyce@sfu.ca>
2022-06-27 09:50:09 -0700

    resync database schema

---
+++ migrations/2022/06/Version20220627164850.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220627164850 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE artistic_statement CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE artwork CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE artwork_category CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE artwork_contribution CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE artwork_role CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE blog_page CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE in_menu in_menu TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE blog_post CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE blog_post_category CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE blog_post_status CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE comment CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE comment_note CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE comment_status CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE element CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE location CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE media_file CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE media_file_category CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE media_file_field CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE organization CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE person CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE project CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE project_category CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE project_contribution CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE project_page CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE project_role CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE venue CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE venue_category CHANGE created created DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE updated updated DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE artistic_statement CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE artwork CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE artwork_category CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE artwork_contribution CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE artwork_role CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE blog_page CHANGE in_menu in_menu TINYINT(1) DEFAULT \'1\' NOT NULL, CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE blog_post CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE blog_post_category CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE blog_post_status CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE comment CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE comment_note CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE comment_status CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE element CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE location CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE media_file CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE media_file_category CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE media_file_field CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE organization CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE person CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE project CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE project_category CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE project_contribution CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE project_page CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE project_role CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE venue CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE venue_category CHANGE created created DATETIME NOT NULL, CHANGE updated updated DATETIME NOT NULL');
+    }
+}
